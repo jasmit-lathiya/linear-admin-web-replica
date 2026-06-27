fac6391776ca8beceb4258137ef5984f4295b66e
Jasmit Lathiya <jasmitlathiya@JaSus-MacBook-Pro.local>
2026-06-27 17:29:05 +0530

    Redirect to contact page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { Routes, Route } from "react-router-dom";
-import MainLayout from "./layouts/MainLayout";
-import Contacts from "./pages/contacts";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import Contacts from './pages/contacts';
 
 function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
-        <Route path="/" element={<div>Home</div>} />
         <Route path="/contacts" element={<Contacts />} />
-        <Route path="*" element={<div>404</div>} />
+        <Route path="*" element={<Navigate to="/contacts" />} />
       </Route>
     </Routes>
   );
